7c1e97133671cd0f59930991fa725175534cc2a5
unknown <fabiohrbarbosa@gmail.com>
2026-04-08 11:21:33 -0300

    fix: fix login and register logic

--- app.js
+++ app.js
@@ -1,59 +1,64 @@
-const express = require('express');
-const { engine } = require('express-handlebars');
-const mongoose = require('mongoose');
+const express = require("express");
+const { engine } = require("express-handlebars");
+const mongoose = require("mongoose");
 const app = express();
-const path = require('path');
-const user = require('./routes/user');
-const session = require('express-session');
-const flash = require('connect-flash');
-const Passport = require('passport');
-require('./config/auth')(Passport);
+const path = require("path");
+const user = require("./routes/user");
+const session = require("express-session");
+const flash = require("connect-flash");
+const Passport = require("passport");
+require("./config/auth")(Passport);
 const db = require("./config/db");
-require('dotenv').config();
+require("dotenv").config();
 
 // Config
-    // sessão
-        app.use(session( {
-            secret: process.env.SECRET || "hashdeteste",
-            resave: true,
-            saveUninitialized: true
-        }));
-        app.use(Passport.initialize());
-        app.use(Passport.session())
-        app.use(flash());
-    // Middleware
-        app.use((req, res, next) => {
-            res.locals.success_msg = req.flash("success_msg")
-            res.locals.error_msg = req.flash("error_msg")
-            res.locals.error = req.flash("error")
-            res.locals.user = req.user || null
-            next()
-        })
-    // Body-Parser
-        app.use(express.urlencoded({extended: true}));
-        app.use(express.json());
-    // handlebars
-        app.engine('handlebars', engine({defaultLayout: 'main'}));
-        app.set('view engine', 'handlebars');
-    // Mongoose
-        mongoose.connect(db.mongoURI).then(() => {
-            console.log("Conectado ao mongo.")
-        }).catch((err) => {
-            console.log("Ocorreu um erro ao tentar se conectar ao mongo:" + err )
-        });
-    // public static files
-        app.use(express.static(path.join(__dirname, "public")))
+// sessão
+app.use(
+  session({
+    secret: process.env.SECRET || "hashdeteste",
+    resave: true,
+    saveUninitialized: true,
+  }),
+);
+app.use(Passport.initialize());
+app.use(Passport.session());
+app.use(flash());
+// Middleware
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  res.locals.user = req.user || null;
+  next();
+});
+// Body-Parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// handlebars
+app.engine("handlebars", engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+// Mongoose
+mongoose
+  .connect(db.mongoURI)
+  .then(() => {
+    console.log("Conectado ao mongo.");
+  })
+  .catch((err) => {
+    console.log("Ocorreu um erro ao tentar se conectar ao mongo:" + err);
+  });
+// public static files
+app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
-    app.get('/', (req, res) => {
-        res.redirect('/user/register')
-    })
+app.get("/", (req, res) => {
+  res.redirect("/user/login");
+});
 
-    app.use("/user", user);
+app.use("/user", user);
 
 // outros
 const port = 5678;
 const PORT = process.env.PORT || port;
 app.listen(PORT, () => {
-    console.log("Servidor rodando.")
-})
+  console.log("Servidor rodando.");
+});
--- config/auth.js
+++ config/auth.js
@@ -1,40 +1,44 @@
-const localStrategy = require('passport-local');
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const localStrategy = require("passport-local");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 //model de usuário
-require('../models/User');
-const User = mongoose.model('user');
+require("../models/User");
+const User = mongoose.model("user");
 
-module.exports = function(passport) {
+module.exports = function (passport) {
+  passport.use(
+    new localStrategy(
+      { usernameField: "username", passwordField: "password" },
+      (username, password, done) => {
+        User.findOne({ username: username }).then((user) => {
+          if (!user) {
+            return done(null, false, { message: "Esta conta não existe!" });
+          }
 
-    passport.use(new localStrategy({usernameField: 'email', passwordField: 'password'}, (email, password, done) => {
-
-        User.findOne({email: email}).then((user) => {
-            if (!user) {
-                return done(null, false, {message: "Esta conta não existe!"})
+          bcrypt.compare(password, user.hash, (erro, batem) => {
+            if (batem) {
+              return done(null, user);
+            } else {
+              return done(null, { message: "Senha incorreta." });
             }
+          });
+        });
+      },
+    ),
+  );
 
-            bcrypt.compare(password, user.hash, (erro, batem) => {
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
 
-                if (batem) {
-                    return done(null, user)
-                } else {
-                    return done(null, {message: "Senha incorreta."})
-                }
-            })
-        })
-    }))
-
-    passport.serializeUser((user, done) => {
-        done(null, user._id)
-    })
-
-    passport.deserializeUser((id, done) => {
-        User.findById(id).then(user => {
-            done(null, user)
-        }).catch(err => {
-            done(err, null)
-        })
-    })
-}
+  passport.deserializeUser((id, done) => {
+    User.findById(id)
+      .then((user) => {
+        done(null, user);
+      })
+      .catch((err) => {
+        done(err, null);
+      });
+  });
+};
