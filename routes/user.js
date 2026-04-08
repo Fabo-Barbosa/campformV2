873a6abb29bafb86f904a685619948ccc731609f
Fabo-Barbosa <fabiohrbarbosa@gmail.com>
2026-04-07 23:32:01 -0300

    feat: register and login logic structure

---
+++ routes/user.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+require('../models/User');
+const UserModel = mongoose.model('user');
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+
+router.get('/register', (req, res) => {
+    res.render("user/register")
+});
+
+router.post('/register', (req, res) => {
+    var erros = []
+
+    if (req.body.password.length < 8) {
+        erros.push({texto: "A senha não possui a quantidade de caracteres apropriada."})
+    }
+
+    if (req.body.password != req.body.password2) {passport
+        erros.push({texto: "Senha repetida incorreta."})
+    }
+
+    if (erros.length > 0) {
+        res.render('user/register', {erros: erros})
+    } else {
+        UserModel.findOne({username: req.body.username}).then((user) => {
+            if (user) {
+                req.flash("error_msg", "Já existe uma conta cadastrada com esse email.")
+                res.redirect("/user/register")
+            } else {
+                UserModel.findOne({nickname: req.body.nickname}).then((user) => {
+                    if (user) {
+                        req.flash("error_msg", "Já existe uma conta cadastrada com esse email.")
+                        res.redirect("/user/register")
+                    } else {
+                        const newUser = UserModel({ 
+                            name: req.body.name,
+                            email: req.body.email,
+                            hash: req.body.password,
+                            nickname: req.body.nickname,
+                            nationality: req.body.nationality
+                        })
+
+                        bcrypt.genSalt(10, (erro, salt) => {
+                            bcrypt.hash(newUser.hash, salt, (erro, hash) => {
+                                if (erro) {
+                                    req.flash("error_msg", "Houve um erro durante o salvamento do usuário.")
+                                    res.redirect("/")
+                                }
+
+                                newUser.hash = hash
+                                newUser.save().then(() => {
+                                    req.flash("success_msg", "Cadastro realizado com sucesso.")
+                                    res.redirect('/')
+                                }).catch((err) => {
+                                    req.flash("error_msg", "Ocorreu um erro interno.")
+                                    res.redirect('/user/register')
+                                })
+                            })
+                        })
+
+                    }
+                }).catch((err) => {
+                    req.flash("error_msg", "Ocorreu um erro no servidor: ", err)
+                    res.redirect("/")
+                })
+            }
+        }).catch((err) => {
+            req.flash("error_msg", "Ocorreu um erro no servidor: ", err)
+            res.redirect("/")
+        })
+    }
+});
+
+router.get('/login', (req, res) => {
+    res.render("user/login")
+});
+
+router.post('/login', (req, res, next) => {
+    passport.authenticate('local', {
+        successRedirect: "/",
+        failureRedirect: "/user/login",
+        failureFlash: true
+    })(req, res, next)
+});
+
+router.get("/logout", (req, res, next) => {
+    req.logout(function(err) {
+        if(err) {return next(err)}
+        req.flash("success_msg", "Deslogado")
+        req.session.destroy()
+        res.redirect('/')
+    });
+});
+
+
+module.exports = router;
